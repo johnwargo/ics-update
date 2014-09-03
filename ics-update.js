f9592807bbe4ab1fc77ffa4ae5b741d4290cc25a
John M. Wargo <john@johnwargo.com>
2014-09-03 09:18:55 -0400

    Initial load

---
+++ ics-update.js
@@ -0,0 +1,345 @@
+#! /usr/bin/env node
+
+/* jshint browser: true */
+
+/* *******************************************
+ * .ics File Update
+ * by John M. Wargo
+ * www.johnwargo.com
+ ********************************************/
+//TODO: allow debug mode to be enabled on the command line
+var blankStr = '';
+var helpFile = 'help.txt';
+var meetingFileOut = 'meeting.ics';
+var theStars = "***************************************";
+
+//External modules we're using
+var colors = require('colors');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var sys = require('sys');
+var uid = require('node-uuid');
+
+colors.setTheme({
+  info: 'grey',
+  help: 'green',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+//----------------------------------------------------------------------
+// Dispaly the help file
+//----------------------------------------------------------------------
+function displayHelp() {
+  //Then display the help file contents
+  var raw = fs.readFileSync(path.join(__dirname, helpFile)).toString('utf8');
+  console.log(raw);
+  console.log();
+  process.exit(1);
+}
+
+//----------------------------------------------------------------------
+// List command line arguments to the console
+//----------------------------------------------------------------------
+function listArgs() {
+  //Get the command arguments
+  var userArgs = process.argv;
+  //Do we have any?
+  var numArgs = userArgs.length;
+  if (numArgs > 0) {
+    console.log("Command line arguments");
+    //Then write them to the console
+    for (var i = 0; i < numArgs; i++) {
+      console.log(i + ": " + userArgs[i]);
+    }
+    console.log();
+  }
+}
+
+//----------------------------------------------------------------------
+// doLog
+// Write to the console, only if debugMode is enabled (true)
+//----------------------------------------------------------------------
+var debugMode = false;
+//----------------------------------------------------------------------
+function doLog(msgText) {
+  if (debugMode) {
+    console.log(msgText);
+  }
+}
+
+//----------------------------------------------------------------------
+// Left pad a string to the specified size
+//----------------------------------------------------------------------
+function pad(num, size) {
+  var s = num + blankStr;
+  while (s.length < size) s = "0" + s;
+  return s;
+}
+
+//----------------------------------------------------------------------
+// Determine the user's home folder, varies per OS.
+//----------------------------------------------------------------------
+function getHomefolder() {
+  var home_folder;
+  //Get the environment variables for the system
+  var theEnv = process.env;
+  doLog("Getting home folder location");
+  //The home folder variable varies per OS
+  if (isWindows) {
+    doLog("Running on Windows");
+    //Set the default home folder for Windows
+    home_folder = theEnv.USERPROFILE;
+  } else {
+    doLog("Runnning on a Linux variant");
+    //Home folder for OS X and Linux
+    home_folder = theEnv.HOME;
+  }
+  //Do we have a value?
+  if (home_folder.length > 0) {
+    //Return the home folder
+    return home_folder;
+  } else {
+    console.error("Unable to determine home folder".error);
+    process.exit(1);
+  }
+}
+
+//----------------------------------------------------------------------
+// Process the ICS File
+// Update information in the ICS file for the current appointment
+//----------------------------------------------------------------------
+function processICSFile(icsFilePath) {
+
+  var colon = ':';
+  var theDate = new Date();
+  var endDate, homeFolder, startDate, theTimeStr, theDateStr, theTimeStampStr, theUID, timeZone;
+
+  //--------------------------------------------------------------------
+  //Locate the array item that begins with the specified string, we'll
+  //replace the whole line later.
+  //--------------------------------------------------------------------
+  function getIndex(theLines, searchStr) {
+    var numLines = theLines.length;
+    if (numLines > 0) {
+      //Do we even have lines?
+      for (var i = 0; i < numLines; i++) {
+        //Does the line contain the serch string in the first position?
+        if (theLines[i].search(searchStr) === 0) {
+          //yes? Then we have a match
+          return i;
+        }
+      }
+      //We didn't find one, so return -1
+      return -1;
+    } else {
+      //no lines to search
+      return -1;
+    }
+  }
+
+  //--------------------------------------------------------------------
+  //Find the search string in the list and replace it with a new line
+  //--------------------------------------------------------------------
+  function updateList(theLines, searchStr, newVal) {
+    var theIndex = getIndex(theLines, searchStr);
+    //Were we able to find it?
+    if (theIndex > -1) {
+      //Update the line from the file
+      theLines[theIndex] = newVal + "\n";
+    } else {
+      console.log("\nUnable to locate meeting value %s\n".error, searchStr);
+      process.exit(1);
+    }
+  }
+
+  //--------------------------------------------------------------------
+  //Get the time zone for the appointment from the existing ICS file
+  //--------------------------------------------------------------------
+  function getTimeZone(theLines) {
+    var theIndex = getIndex(theLines, "TZID:");
+    //Were we able to find it?
+    if (theIndex > -1) {
+      //Get the line from the file
+      var tmpStr = theLines[theIndex];
+      //Get everything past the search string
+      return tmpStr.substr(5, 99).trim();
+    } else {
+      console.log("\nUnable to determine meeting time zone\n".error);
+      process.exit(1);
+    }
+  }
+
+  //--------------------------------------------------------------------
+  //here's where we start working with the .ics file
+  //--------------------------------------------------------------------
+  //get the user's home folder path
+  homeFolder = getHomefolder();
+  console.log("Home folder: %s", homeFolder);
+
+  //Resolve the file path to an absolute path
+  var sourceFile = path.normalize(icsFilePath);
+
+  //Get the ics file name
+  var icsFileName = path.basename(sourceFile);
+  //Check to see if we just got a file name, if so, add home folder to it
+  if (icsFileName == sourceFile) {
+    //Build the target folder using the user's home folder
+    sourceFile = path.join(homeFolder, icsFileName);
+  }
+  //Tell the user what file we're going to be updating
+  console.log("Source file: %s", sourceFile);
+
+  //Figure out the output file path
+  var outputFile = path.join(homeFolder, meetingFileOut);
+  //Tell the user where we're writing the output file
+  console.log("Output file: %s", outputFile);
+
+  //Make sure the file actually exists
+  if (!fs.existsSync(sourceFile)) {
+    //the file doesn't exist, so we can't use it
+    console.log("\nSpecified file (%s) does not exist\n".error, sourceFile);
+    process.exit(1);
+  }
+
+  console.log("Target .ics file: %s", icsFileName);
+  //Make sure we actually have a ,ics file to work with
+  if (path.extname(icsFileName) !== '.ics') {
+    console.log("\nCannot process file, '%s' is not an .ics file\n".error, icsFileName);
+    process.exit(1);
+  }
+
+  //=================================================================
+  //Read the file
+  //=================================================================
+  console.log('Reading .ics file');
+  //Read the file, parse each line into an array
+  var theLines = fs.readFileSync(sourceFile).toString().split("\n");
+
+  //Now that we have the .ics file, use it to determine the meeting time zone
+  timeZone = getTimeZone(theLines);
+  console.log("Time zone: '%s'", timeZone);
+
+  //=================================================================
+  //Replace the stuff we need to replace in the .ics file
+  //=================================================================
+  console.log('Updating .ics file contents');
+
+  //First do the meeting start date/time
+  theDateStr = theDate.getFullYear() + pad(theDate.getMonth() + 1, 2) + pad(theDate.getDate(), 2);
+  theTimeStr = 'T' + pad(theDate.getHours(), 2) + '0000';
+  //DTSTART;TZID=America/New_York:20140307T120000
+  startDate = 'DTSTART;TZID=' + timeZone + colon + theDateStr + theTimeStr;
+  console.log('Meeting Start: %s', startDate);
+  updateList(theLines, "DTSTART;", startDate);
+
+  //Next do the meeting end date/time
+  theTimeStr = 'T' + pad(theDate.getHours() + 1, 2) + '0000';
+  //DTEND;TZID=America/New_York:20140307T130000
+  endDate = 'DTEND;TZID=' + timeZone + colon + theDateStr + theTimeStr;
+  console.log('Meeting End: %s', endDate);
+  updateList(theLines, "DTEND;", endDate);
+
+  //Data/Time stamp (whatever that is)
+  //DTSTAMP:20140314T100000Z
+  theTimeStampStr = 'DTSTAMP:' + theDateStr + theTimeStr + 'Z';
+  console.log('Timestamp: %s', theTimeStampStr);
+  updateList(theLines, "DTSTAMP:", theTimeStampStr);
+
+  //Finally the unique identifier for the meeting request
+  //Allows us to use to use the same file over and over again to generate
+  //a 'new' meeting every time
+  //UID:B1120E2C-E115-469F-9042-F5505615F079
+  theUID = 'UID:' + uid.v1().toUpperCase();
+  console.log('GUID: %s', theUID);
+  updateList(theLines, "UID:", theUID);
+
+  //=================================================================
+  //Write the changes back to disk
+  //=================================================================
+  console.log("Writing to %s", outputFile);
+
+  //First, write out the first line of the file
+  fs.writeFileSync(outputFile, theLines[0]);
+  //Then append the contents of the ics file to the output file
+  var numLines = theLines.length;
+  for (var i = 1; i < numLines; i++) {
+    //Now append the rest of the file
+    fs.appendFileSync(outputFile, theLines[i]);
+  }
+  //Mark the file as having been processed by this script
+  fs.appendFileSync(outputFile, "Note: .ics file created by ics-update (https://github.com/johnwargo/ics-update)");
+
+  //=================================================================
+  // Now launch the .ics file we just updated
+  //=================================================================
+  console.log("Launching %s", outputFile);
+  var child;
+  child = exec("open " + outputFile, function (error, stdout, stderr) {
+    sys.print('stdout: ' + stdout);
+    sys.print('stderr: ' + stderr);
+    if (error !== null) {
+      console.log("\nexec error: %s\n".error, error);
+      process.exit(1);
+    }
+  });
+}
+
+//=================================================================
+//Here's where we start doing stuff...
+//=================================================================
+//Write out what we're running
+console.log("\n%s".help, theStars);
+console.log(" .ics File Update".help);
+console.log(theStars.help);
+
+if (debugMode) {
+  console.log("Debug mode is enabled\n".help);
+  //Write the argument list to the console
+  listArgs();
+}
+
+//Are we running on Windows? It matters for some things
+var isWindows = (os.type().indexOf('Win') === 0);
+if (isWindows) {
+  //Todo: Test this on Windows
+  console.log("\nThis application is currently only supported on Macintosh OS X\n".error);
+  process.exit(1);
+}
+
+//=================================================================
+//First lets sort out the command line arguments
+//=================================================================
+var userArgs;
+//Is the first item 'node'? then we're testing
+if (process.argv[0].toLowerCase() == 'node') {
+  //whack the first two items off of the list of arguments
+  //This removes the node entry as well as the module name entry (the
+  //program we're running)
+  userArgs = process.argv.slice(2);
+} else {
+  //whack the first item off of the list of arguments
+  //This removes just the module name entry
+  userArgs = process.argv.slice(1);
+}
+//now parse the command line options
+var numArgs = userArgs.length;
+if (numArgs > 0) {
+  //First paranater is the file path we need
+  processICSFile(userArgs[0]);
+} else {
+  //Missing command line oarguments
+  console.log("\nMissing command-line argument(s)\n".error);
+  displayHelp();
+}
+
+//========================================================================
+// Finished - if we got this far, it must have worked
+//========================================================================
+console.log(theStars.help);
+console.log("All done!".help);
+console.log(theStars.help);
+console.log();
---
+++ readme.md
@@ -0,0 +1,74 @@
+.ics File Update
+====================
+
+This is a simple node module that updates a simple calendar entry file (an .ics file) with the current date then launches it. The purpose of this module is to provide users with an easy way to populate a meeting invite, export it to a .ics file then use the module to prepare it for reuse for creating subsequent meeting requests.
+
+Many sales people and other professionals regularly invite team members, customers or partners to conference calls or online meetings. While there are often email client plugins that automate some of this process, most users store their conference information (dial-up, URL) somewhere (perhaps in a text file or as a contact in their address book). When they create a new meeting invitation, they'll open the stored conference information and copy and paste it into the new meeting request. This can become quite tedious, especially if you complete these steps on a regular basis.
+
+A longtime friend and colleague recently asked me to create a process he could use to take a pre-populated meeting invitation (one that had his conference dial-up and so on already populated in the body of the meeting request) and update it with the current date (and future time) then open it so he could easily add the meeting subject, adjust the meeting time and invite participants. I could have made a native application or a email client plugin, but it would be hard to deliver a cross-platform solution or one that didn't leave out popular email clients. 
+
+I'd been working a bit with NodeJS and wanted to create (and publish) a node module, so I decided to make this thing using NodeJS. It should work on Windows, but I have not tested it (yet).
+
+How it Works
+====================
+The module takes an existing iCalendar (.ics) file, updates it and uses the system's shell to launch it using the default application. This allows users to send the meeting request using their default calendar client. The [iCalendar file](http://en.wikipedia.org/wiki/ICalendar) is essentially just a text file, you can make your own or export one from your existing calendar program. 
+
+Create an appointment, populate the subject, body fields plus any other ones you want pre-populated then export it as an .ics file to your system's local hard drive. I recommend putting it in your Documents or Home folder.
+
+Once you have the .ics file, execute the ics-update command and pass it a path pointing to the .ics file you saved. The program will read the file, update the existing meeting request to point to the current date and adjust the time to two hours (rounded) from the current time then launch it using the system's default calendar program.
+
+Once launched, the meeting request will open in edit mode - you should then adjust the subject as needed, add participants and change the date and time to the correct value for your meeting and send the invitation out. 
+
+That's it.
+
+In reality, I don't expect that you will want to regularly open a terminal window to make a meeting invite, so what my colleague did was use the Macintosh OS X Automator program to make an application or a service you can launch to execute the command (with the appropriate .ics file path and so on). This makes it easier (and more visual) to execute this when needed. On Windows, once I've confirmed this will work on Windows, you can easily craft a batch file that launches the module with the appropriate command line parameter(s).
+
+What this approach supports is
+
+
+Installation
+====================
+Install this module using npm by opening a terminal window and executing the following command:
+
+Windows:
+
+	npm install -g ics-update
+
+Macintosh OS X:
+
+	sudo npm install -g ics-update
+
+If you've downloaded this module's code from GitHub, you can install the module by extracting the files then opening a terminal window and navigating to the folder where this file is located and issuing the following command:
+
+Windows:
+
+	npm install -g
+
+Macintosh OS X:
+
+	sudo npm install -g
+
+That's it, that's all there is to installing the module.
+
+Usage
+====================
+To use the module, open a terminal window and execute the following command:
+
+    ics-update path_to_ics_file
+
+The path_to_ics_file is a parameter that specifies a file path to a valid .ics file. For example, if the .ics file was a file called call.ics located in the user's Documents folder, you could process the file using the following command:
+
+    ics-update /Users/user_name/Documents/call.ics
+    
+The file path pointing to the .ics file can be absolute or relative, the following work as well:
+
+    ics-update ../test/call.ics
+    
+if the .ics file is in the user's home folder, you can simply pass the file name to the call to make-ics:
+
+    ics-update ics_file_name.ics
+
+When the module sees that it only has a file name, it builds a full path using the home folder and looks for the file there.
+
+***
+By [John M. Wargo](http://www.johnwargo.com) - if you like and/or use this module, why not pick up [one of my books](http://www.johnwargobooks.com)?
